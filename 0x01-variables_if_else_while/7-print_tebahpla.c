bc2bbffbf3fe1be16e519e650945b97aa3d154d1
DOHA <dohasbaa@gmail.com>
2023-03-21 17:58:37 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char i;
+
+	i = 'z';
+	while
+		(i >= 'a') {
+			putchar(i);
+			i--;
+		}
+	putchar('\n');
+	return (0);
+}
