018426a07da150537a7f220b1da962368c4c51b4
DOHA <dohasbaa@gmail.com>
2023-03-21 18:21:16 -0700

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,22 +7,13 @@
 
 int main(void)
 {
-	char c;
+	char j;
+	int i;
 
-	int d;
-
-	c = 'a';
-	d = '0';
-	while
-		(d < 10) {
-			putchar(d + '0');
-			d++;
-		}
-	while
-		(c <= 'f') {
-			putchar(c);
-			c++;
-		}
+	for (i = 0; i < 10; i++)
+		putchar(i + '0');
+	for (j = 'a'; j <= 'f'; j++)
+		putchar(j);
 	putchar('\n');
 	return (0);
 }
