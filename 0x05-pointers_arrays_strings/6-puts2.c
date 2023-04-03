b9f54bae5db4bbbff2a4a16a64e346f380bdcf4e
DOHA <dohasbaa@gmail.com>
2023-04-02 18:18:18 -0700

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - function that prints every other character of a string starting with the first one
+ * @str: function parameter
+ * Return: always 0
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
+
