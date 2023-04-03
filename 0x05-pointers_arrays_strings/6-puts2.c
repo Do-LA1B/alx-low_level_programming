1dd81f2e86ffe1e3ad5b43d86833398bbde0d42c
DOHA <dohasbaa@gmail.com>
2023-04-02 18:24:43 -0700

    puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * puts2 - function that prints every other character of a string starting with the first one
+ * puts2 - function that prints every other character
  * @str: function parameter
  * Return: always 0
  */
