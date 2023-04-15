52316a5431369b0d1b75b87c4496c713528eb959
DOHA <dohasbaa@gmail.com>
2023-04-14 19:23:28 -0700

    silence

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments
+ * @argc: number of command line arguments
+ * @argv: pointer to an array of command line arguments
+ * Return: 0-success , non-zero-fail
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
